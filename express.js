abd3d407797c0af2c5f1debb111a1df642d4284c
bella148 <bella148@morris.umn.edu>
2019-03-04 16:35:42 -0600

    Continue progress

--- express.js
+++ express.js
@@ -1,3 +1,4 @@
+Promise = require('bluebird');
 var express=require('express'),
 app = express(),
 port = process.env.PORT || 1337;
@@ -8,36 +9,56 @@ var mysql=require("mysql");
 credentials.host="ids";
 credentials.database = "dndatabase";
 var connection = mysql.createConnection(credentials);
-
-//Attempt to connect
-connection.connect(function(err){
-  if(err){
-    console.log("Problems with MySQL: "+err);
-  } else {
-    console.log("Connected to Database.");
-  }
-});
-
-//If connected, get the buttons
-connection.query('SELECT * FROM till_buttons;',function(err,rows,fields){
-  if(err){
-    console.log('Error looking up databases', err);
-  } else {
-    console.log('Returned values were ',rows);
-}
-});
-connection.end()
-console.log("All done now.");
-
-
-// Make query to server to get all the buttons as JSON objects
-// buttonID, x, y, width, label, invId
 var buttons;
 
 
-app.use(express.static(__dirname + '/public'));
-app.get("/buttons",function(req,res){
-  res.send(buttons);
-});
+var connectToDatabase = function()
+{
+
+	return new Promise(function()
+	{
+		//Attempt to connect
+		connection.connect(function(err)
+		{	
+		  if(err)
+		{
+		    console.log("Problems with MySQL: "+err);
+		
+		  } else {
+		    console.log("Connected to Database.");
+		  }
+		});
+	});
+	
+}
+
+var getDBButtons = function(temp)
+{
+	console.log("We got here!");
+	//If connected, get the buttons
+	connection.query('SELECT * FROM till_buttons;',function(err,rows,fields){
+	if(err){
+		console.log('Error looking up databases', err);
+	} else {
+		console.log('Returned values were ',rows);
+			connection.end()
+			console.log("All done now.");
+			return Promise.resolve(rows);
+		}
+	});
+	
+}
+
+var processDBButtons = function(rows)
+{
+	var buttons = rows;
+	app.use(express.static(__dirname + '/public'));
+	app.get("/buttons",function(req,res){
+	  res.send(buttons);
+	});
+}
+
+var load = connectToDatabase().then(getDBButtons).then(processDBButtons).catch(function(err){console.log("Big oops", err)});
 
 app.listen(port);
+
